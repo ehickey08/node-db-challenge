27869f8ad5d5ce5685ec90648b05b31425d368d8
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2019-08-16 13:28:24 -0400

    Add .unqiue to resource name

--- data/migrations/20190816111253__projects.js
+++ data/migrations/20190816111253__projects.js
@@ -25,7 +25,7 @@ exports.up = function(knex) {
         })
         .createTable('resources', tbl => {
             tbl.increments();
-            tbl.string('name', 255).notNullable().unqiue();
+            tbl.string('name', 255).notNullable().unique();
             tbl.string('description', 255);
         })
         .createTable('contexts', tbl => {
